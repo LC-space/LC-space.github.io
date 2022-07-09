10fac4b6f6e7d94640ae0bf4cd5f5c2bd225e056
LC-space <LCspace001@Gmail.com>
2022-07-09 10:25:05 +0800

    更新新闻

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,7 @@ Theme by: WebThemez.com
 Note: Please use our back link in your site
 */
 $( function() {
-        var endDate = "June 5, 2022 00:00:00";
+        var endDate = "July 24, 2022 00:00:00";
 
         $('.countdown.simple').countdown({ date: endDate });
 
